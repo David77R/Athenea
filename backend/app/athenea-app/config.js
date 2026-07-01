a56f88aeda260835858e67001415eb6a9936609d
David77R <davidker.7.9@gmail.com>
2026-06-30 22:23:48 -0400

    generacion de apk

--- backend/app/athenea-app/config.js
+++ backend/app/athenea-app/config.js
@@ -1,16 +1,23 @@
 const CONFIG = {
-  //API_URL:'http://10.65.55.206:3001',
-  //CLINICAL_URL: 'http://10.65.55.206:3002', 
-   //IA_URL: 'http://10.65.55.206:3003',
+  // ── Render 
+   API_URL: 'https://athenea-auth.onrender.com',
+   CLINICAL_URL: 'https://athenea-clinical.onrender.com',
+   IA_URL: 'https://athenea-ia.onrender.com',
 
+  // ── WiFi casa
+  //API_URL: 'http://192.168.0.196:3001',
+  //CLINICAL_URL: 'http://192.168.0.196:3002',
+  //IA_URL: 'http://192.168.0.196:3003',
 
-  API_URL:'http://192.168.0.196:3001',
-  CLINICAL_URL: 'http://192.168.0.196:3002', 
-   IA_URL: 'http://192.168.0.196:3003',
- 
-  // API_URL:'http://172.22.34.206:3001',
-  //CLINICAL_URL: 'http://172.22.34.206:3002', 
-   // IA_URL: 'http://172.22.34.206:3003',
-  };
+  // datos
+  // API_URL: 'http://10.65.55.206:3001',
+  // CLINICAL_URL: 'http://10.65.55.206:3002',
+  // IA_URL: 'http://10.65.55.206:3003',
+
+  // eve
+  // API_URL: 'http://172.22.34.206:3001',
+  // CLINICAL_URL: 'http://172.22.34.206:3002',
+  // IA_URL: 'http://172.22.34.206:3003',
+};
 
 export default CONFIG;
