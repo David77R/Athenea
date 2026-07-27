882df674713bdaf07ff02ecc3abad0eaee01a9ee
David77R <davidker.7.9@gmail.com>
2026-07-27 00:06:01 -0400

    apk produccion render

--- backend/app/athenea-app/config.js
+++ backend/app/athenea-app/config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
   // ── Render 
   
-  //API_URL: 'https://athenea-auth.onrender.com',
-  // CLINICAL_URL: 'https://athenea-clinical.onrender.com',
-   //IA_URL: 'https://athenea-ia.onrender.com',
+  API_URL: 'https://athenea-auth.onrender.com',
+   CLINICAL_URL: 'https://athenea-clinical.onrender.com',
+   IA_URL: 'https://athenea-ia.onrender.com',
 
   // ── WiFi casa
-  API_URL: 'http://192.168.0.196:3001',
-  CLINICAL_URL: 'http://192.168.0.196:3002',
-  IA_URL: 'http://192.168.0.196:3003',
+  //API_URL: 'http://192.168.0.196:3001',
+ // CLINICAL_URL: 'http://192.168.0.196:3002',
+ // IA_URL: 'http://192.168.0.196:3003',
 
   // datos
   // API_URL: 'http://10.65.55.206:3001',
